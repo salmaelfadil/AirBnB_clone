f42511da20613d75c8d44ff4dc8c02a8e5a49b19
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-09-16 05:16:36 -0700

    9

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,12 @@ import models
 class BaseModel():
     """BaseModel class from which other classes will inheret"""
     def __init__(self, *args, **kwargs):
-        """starting point of the class"""
+        """
+        Starting point of the class
+        Args:
+            *args: arguments .
+            **kwargs: Keys and values of attributes.
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -25,17 +30,17 @@ class BaseModel():
             models.storage.new(self)
 
     def __str__(self):
-        """string representation of base model"""
+        """String representation of base model"""
         return "[{}] ({}) {}".format(
                 self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """save basemodel method"""
+        """Save basemodel method"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """save basemodel to dictionary function"""
+        """Save basemodel to dictionary function"""
         my_dict = self.__dict__.copy()
         my_dict["__class__"] = self.__class__.__name__
         my_dict["created_at"] = self.created_at.isoformat()
