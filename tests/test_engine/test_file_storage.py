775af6a0a5790682b10704b4863f135b8e064a73
Eng-Esraa-shamaa <esra2asmaibrahim@gmail.com>
2023-09-16 07:30:53 -0700

    testsss

--- tests/test_engine/test_file_storage.py
+++ tests/test_engine/test_file_storage.py
@@ -30,6 +30,93 @@ class TestFileStorage(unittest.TestCase):
         with self.assertRaises(TypeError):
             FileStorage(None)
 
+    @classmethod
+    def setup(cls):
+        """
+        Preps for testing
+        """
+        try:
+            os.rename("file.json", "tmp")
+        except Exception:
+            pass
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Restoring file
+        """
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except Exception:
+            pass
+
+    def test_inst(self):
+        """
+        Testing instance type
+        """
+        storage = FileStorage()
+        self.assertIsInstance(storage, FileStorage)
+
+    def test_storage_all(self):
+        """
+        Testing the all() method
+        """
+        storage = FileStorage()
+        dic = storage.all()
+        self.assertIsInstance(dic, dict)
+
+    def test_new(self):
+        """
+        Testing the new() method
+        """
+        city = City()
+        state = State()
+        amenity = Amenity()
+        user = User()
+        review = Review()
+        place = Place()
+        base = BaseModel()
+        self.assertIn("City." + city.id, models.storage.all().keys())
+        self.assertIn("State." + state.id, models.storage.all().keys())
+        self.assertIn("Amenity." + amenity.id, models.storage.all().keys())
+        self.assertIn("User." + user.id, models.storage.all().keys())
+        self.assertIn("Review." + review.id, models.storage.all().keys())
+        self.assertIn("Place." + place.id, models.storage.all().keys())
+        self.assertIn("BaseModel." + base.id, models.storage.all().keys())
+        self.assertIn(city, models.storage.all().values())
+        self.assertIn(state, models.storage.all().values())
+        self.assertIn(amenity, models.storage.all().values())
+        self.assertIn(user, models.storage.all().values())
+        self.assertIn(review, models.storage.all().values())
+        self.assertIn(place, models.storage.all().values())
+        self.assertIn(base, models.storage.all().values())
+
+    def test_save(self):
+        """
+        Testing the save() method
+        """
+        city = City()
+        state = State()
+        amenity = Amenity()
+        user = User()
+        review = Review()
+        place = Place()
+        base = BaseModel()
+        models.storage.save()
+        with open("file.json", "r") as file:
+            f_contents = file.read()
+            self.assertIn("City." + city.id, f_contents)
+            self.assertIn("State." + state.id, f_contents)
+            self.assertIn("Amenity." + amenity.id, f_contents)
+            self.assertIn("User." + user.id, f_contents)
+            self.assertIn("Review." + review.id, f_contents)
+            self.assertIn("Place." + place.id, f_contents)
+            self.assertIn("BaseModel." + base.id, f_contents)
+
     def test_all_method(self):
         """Tests the return type of all method"""
         self.assertIsInstance(self.storage.all(), dict)
